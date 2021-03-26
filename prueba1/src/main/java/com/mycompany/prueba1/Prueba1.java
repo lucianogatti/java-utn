c7c8278f95ec80f343baf0fdb246ec5fbb4486a9
lucia <lucia@DESKTOP-3LS9A1P.fibertel.com.ar>
2021-03-25 22:36:14 -0300

    Se actualizo prueba 1

--- prueba1/src/main/java/com/mycompany/prueba1/Prueba1.java
+++ prueba1/src/main/java/com/mycompany/prueba1/Prueba1.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.prueba1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author lucia
@@ -14,10 +16,24 @@ public class Prueba1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) { 
+    
+        int a, b;
+        Scanner num = new Scanner(System.in); 
         
-        System.out.println("Hola");
+        System.out.println("Ingrese el primer numero: ");
+        a = num.nextInt();
+        
+        System.out.println("Ingrese el segundo numero: ");
+        b = num.nextInt();
+     
+       int suma = a + b;
+       int resta = a - b;
+       double division =  a / b;
+       int multiplicación = a * b;
+       
+       System.out.println("Suma: " + suma + "Resta: " + resta + "División: "
+       + division + "Multiplicación: " + multiplicación);
     }
     
 }
