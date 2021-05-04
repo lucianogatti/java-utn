01e1ef79e07be7ecb0a23d5f98da8f87c8a63bf7
lucia <lucia@DESKTOP-3LS9A1P.fibertel.com.ar>
2021-05-04 02:13:54 -0300

    punto49

--- Punto59/src/main/java/com/mycompany/punto59/punto49.java
+++ Punto59/src/main/java/com/mycompany/punto59/punto49.java
@@ -3,18 +3,46 @@ package com.mycompany.punto59;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class punto49 {
+
+public class punto49 
+{
    
    public static void main(String[] args)    
    {
-    boolean ciclo = true;
-    while(ciclo = true)        
-    {
-        System.out.print("Hola");
+        Scanner leer = new Scanner (System.in);
+        boolean ciclo = true;
+    
+     while(ciclo = true)        
+        {
+            int opcion; 
+            System.out.println("**SISTEMA ESCOLAR**\nQUE DESEA HACER:\n1. CARGAR DATOS DEL ESTUDIANTE\n2. CARGAR NOTAS\n3. IMPRIMIR LISTA DE ALUMNOS\n4. SALIR");
+            opcion = leer.nextInt();
         
+         switch (opcion)
+            {
+                case 1:/*Cargar datos*/
+                    System.out.println("cargar 1");
+                    break;
+            
+                case 2:
+                    System.out.println("cargar 2");
+                    break;
+           
+                case 3:
+                    System.out.println("cargar 3");
+                    break;
+            
+                case 4:
+                    ciclo = false;
+                    break;
+           
+                default:
+                    System.out.println("No carga");
+                    break;         
+            }             
+        }
     }
-    
-    
-   }
+   
 }
